3a306d51acf83bf1b381fe2bc0e7340cb0d66a48
Xavier Barbosa <xavier.barbosa@backmarket.com>
2018-12-26 16:00:27 +0100

    publish

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+import pathlib
 import versioneer
 
+here = pathlib.Path(__file__).parent
+
+
+def read(f):
+    return (here / f).read_text("utf-8").strip()
+
+
 setup(
-    name='knighted',
+    name="knighted",
     version=versioneer.get_version(),
-    author='Xavier Barbosa',
-    author_email='clint.northwood@gmail.com',
-    description='inject dependencies',
+    author="Xavier Barbosa",
+    author_email="clint.northwood@gmail.com",
+    description="inject dependencies",
+    long_description=read("README.rst"),
     packages=find_packages(),
-    install_requires=[
-        "cached_property"
-    ],
-    extras_require={
-    },
+    install_requires=["cached_property"],
+    extras_require={},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -22,10 +28,10 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules"
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    keywords=['dependency injection', 'composing'],
-    url='http://lab.errorist.xyz/abc/knighted',
-    license='MIT',
-    cmdclass=versioneer.get_cmdclass()
+    keywords=["dependency injection", "composing"],
+    url="http://lab.errorist.xyz/abc/knighted",
+    license="MIT",
+    cmdclass=versioneer.get_cmdclass(),
 )
